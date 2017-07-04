8d8a9fa007d219cf21b5afe68e863118704b6be7
amansawhney <amansawhney@sawhneygames.com>
2017-07-03 20:08:00 -0400

    fixed pulling

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170629230854) do
+ActiveRecord::Schema.define(version: 20170629235347) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 20170629230854) do
     t.string "developer"
     t.string "dilution"
     t.string "iso"
-    t.integer "time"
+    t.float "time"
     t.string "temp"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
