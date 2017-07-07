0fc7abc845005c1b12f40ba3e2e3ad4342b645ed
amansawhney <amansawhney@sawhneygames.com>
2017-07-06 20:00:22 -0400

    switched to cookies for holding old states vs using hidden forms

--- app/controllers/films_controller.rb
+++ app/controllers/films_controller.rb
@@ -1,15 +1,17 @@
 class FilmsController < ApplicationController
 	String name
 	def index
-		@films = Film.where(:name => params[:name], :developer => params[:dev], :iso => params[:iso])
+		@films = Film.where(:name => cookies[:name], :developer => cookies[:dev], :iso => params[:iso])
 	end
 	def iso_select
-		@films = Film.where(:name => params[:name], :developer => params[:dev])
+		cookies[:dev] = params[:dev]
+		@films = Film.where(:name => cookies[:name], :developer => params[:dev])
 	end
 	def film_select
 		@films = Film.all
 	end
 	def dev_select
+		cookies[:name] = params[:name]
 		@films = Film.where(:name => params[:name])
 	end
 end
